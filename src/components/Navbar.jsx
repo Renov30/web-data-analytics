6b47ea2cabcf207ced6d823690d87b80134be096
Renov30 <dalero33ermiawan@gmail.com>
2025-03-12 13:22:19 +0900

    fix(navbar): fix navbar shows after each refresh, fix menu button not in fixed position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,25 +18,23 @@ const Navbar = () => {
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div onClick={handleNav} className="block md:hidden">
-        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      <div onClick={handleNav} className="block fixed right-4 top-10 md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div
+      <ul
         className={
-          !nav
+          nav
             ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "fixed left-[-100%]"
+            : "fixed left-[-100%] top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
         }
       >
         <h1 className="w-full text-3xl font-bold text-[#00df9a] m-8">REACT.</h1>
-        <ul className="p-4 uppercase">
-          <li className="p-4 border-b border-gray-600">Home</li>
-          <li className="p-4 border-b border-gray-600">Company</li>
-          <li className="p-4 border-b border-gray-600">Resources</li>
-          <li className="p-4 border-b border-gray-600">About</li>
-          <li className="p-4 border-b border-gray-600">Contact</li>
-        </ul>
-      </div>
+        <li className="p-4 border-b border-gray-600">Home</li>
+        <li className="p-4 border-b border-gray-600">Company</li>
+        <li className="p-4 border-b border-gray-600">Resources</li>
+        <li className="p-4 border-b border-gray-600">About</li>
+        <li className="p-4 border-b border-gray-600">Contact</li>
+      </ul>
     </div>
   );
 };
